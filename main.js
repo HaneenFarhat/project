4f2af6d5dbbf6329478e4679ae41053c5bf6acaa
HaneenFarhat <haneenfarhat.33@gmail.com>
2022-02-17 17:03:58 +0200

    ggggg

---
+++ main.js
@@ -0,0 +1,243 @@
+
+
+
+
+// const arr1 = ['haneen', 'ahmad', 'adam', 'yazeed', 'rami']
+
+
+// const sayHello = function (arr) {
+
+
+
+//     for (let i = 0; i < arr.length; i++) {
+
+
+//         console.log("Hello " + arr[i]);
+//     }
+// }
+// sayHello(arr1)
+
+
+
+
+
+// let num1 =5
+// let num2 = num1
+
+// num2++
+// //   ps zaad 1
+
+
+// // num+=3 
+// // more then one
+
+
+
+
+
+
+
+
+// //     const formatArr= function (arr) {
+
+
+
+// //         for (let i = 0; i < arr.length; i++) {
+// //             arr[i] = "hello " + arr[i]
+
+// //         }
+// //     }
+// //     formatArr(arr1)
+// // console.log(arr1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // const formatArr= function (arr) {
+
+
+
+// //     for (let i = 0; i < arr.length; i++) {
+// //         arr[i] = arr[i]
+// //         arr1[i]=arr1[i].toUpperCase()
+
+// //     }
+// // }
+// // formatArr(arr1)
+// // console.log(arr1);
+
+
+
+
+
+// const capitalize= function (arr) {
+
+
+
+//     for (let i = 0; i < arr.length; i++) {
+//         arr[i] = arr[i].split('')
+//         arr[i] [0]= arr[i][0].toUpperCase()
+//         arr[i]=arr[i].join('')
+    
+
+//     }
+// }
+// capitalize(arr1)
+// console.log(arr1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //new
+
+
+
+
+// const arr2= ['haneen', 'ahmad', 'adam', 'yazeed', 'rami'] //  ] : for array
+
+// console.log(arr2);
+// arr2.push('sameera') // adding 
+// arr2.splice(3,1,'suad','sadeen') //delete 3 just one thing & add
+// const newArr = arr2.slice(2,arr2.length)
+// console.log(arr2);
+// console.log(newArr);
+
+
+
+
+
+
+
+
+// const user = { firstName : "Haneen",lastName:'Farhat', age: 16} // } : for project
+
+// // console.log(user.firstName);
+
+
+// user.mobileNo ='0532378887'
+
+// // // user.age++   //add
+// // user.age+=3   //add more then one
+
+// const newUser = user
+
+// console.log(user);
+
+
+
+
+
+
+
+
+
+
+//new projet & method
+
+
+
+
+// const user = { 
+//     firstName : "Haneen",
+//     lastName:'Farhat', 
+//     age: 16,
+// isLoggedIn: false,
+// signIn: function () {
+//     this.isLoggIn= true
+    
+// },
+
+
+// } // } : for project
+
+// // console.log(user.firstName);
+
+
+// user.mobileNo ='0532378887'
+
+// // // user.age++   //add
+// // user.age+=3   //add more then one
+
+
+
+// console.log(user);
+// user.signIn()
+//     console.log(user);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const user = { 
+    firstName : "Haneen",
+    lastName:'Farhat', 
+    age: 16,
+isLoggedOut: true,
+signOut: function () {
+    this.isLoggOut= false
+    
+},
+
+
+} // } : for project
+
+// console.log(user.firstName);
+
+
+user.mobileNo ='0532378887'
+
+// // user.age++   //add
+// user.age+=3   //add more then one
+
+
+
+console.log(user);
+user.signOut()
+    console.log(user);
+
